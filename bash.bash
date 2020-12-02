66fcd0466b597f4555bd3c8dc4eb09c7fa0cdd56
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-02 14:54:19 -0300

    alter

---
+++ bash.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+npm init -y
+
+val=$(cat package.json)
+search='"scripts": {'
+replace='"scripts": {
+    "start": "tsc && node dist/server.js",
+    "dev": "ts-node-dev --respawn --transpile-only src/server.ts",'
+echo "${val/$search/$replace}">package.json
+# npm install typescript --save-dev
+# npm install express 
+# npm install @types/express --save-dev
+
+#tsc --init
+
+# npm install ts-node-dev --save-dev
+
+# npm install body-parser
+# npm install @types/body-parser --save-dev
+ 
